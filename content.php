2187e47f994a5e8ff61c27f68ea13e27c52bd928
Agaly Omarov <omaraly971215@gmail.com>
2022-09-21 11:43:08 +0300

    done

--- content.php
+++ content.php
@@ -1,23 +1,5 @@
-<?php
-echo file_get_contents("https://xn----8sbeclb6ai7aqz.xn--p1ai/in-stock/audi/a6/119910");
-?>
-<!-- <script>
-   fetch('index.php', {
-         method: 'POST',
-         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-         },
-         body: JSON.stringify(body)
-      })
-      .then(res => res.text())
-      .then(data => {
-         console.log(data);
-      })
-   console.log(body);
-</script> -->
-
-<!-- setInterval(function(){window.scrollBy(0,1000)},500); -->
-<!-- let articles=document.querySelectorAll('article'); -->
-<!-- let links=[]; -->
-<!-- articles.forEach(function(article,key){links[key]=article.querySelector('a').getAttribute('href')}) -->
+setInterval(function(){window.scrollBy(0,1000)},500);
+let articles=document.querySelectorAll('article');
+let links=[];
+articles.forEach(function(article,key){links[key]=article.querySelector('a').getAttribute('href')})
+console.log(JSON.stringify(links))
